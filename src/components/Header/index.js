b735a8342e92a85b2dfa616b306163c849d25e66
Jared Cabanilla <Jcabanilla@gmail.com>
2019-10-17 21:21:37 -0700

    fixed logo image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./style.css"
 
 function Header(props) {
-  return <header><img className="imgHeader" alt="header" src="/images/header.jpg"></img>
+  return <header><img className="imgHeader" alt="header" src="./images/header.jpg"></img>
   <div className="score">Score: {props.score}</div><div className="hiScore">Hi Score: {props.hiScore}</div></header>;
 }
 
